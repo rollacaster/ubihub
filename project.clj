dd74cdf08295a94785eda833ae478a9e7f4448b1
Thomas Sojka <thomas.sojka@comsysto.com>
2018-04-29 16:00:24 +0200

    Install figwheel

--- project.clj
+++ project.clj
@@ -3,7 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [cider/piggieback "0.3.1"]
+                 [figwheel-sidecar "0.5.16-SNAPSHOT"]]
+  :plugins [[lein-figwheel "0.5.16-SNAPSHOT"]]
   :main ^:skip-aot shopping-list.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+  :cljsbuild {
+    :builds [{:id "dev"
+             :source-paths ["src"]
+             :figwheel true
+             :compiler {:main "shopping-list.core"}}]
+  })
---
+++ src/shopping_list/core.cljs
@@ -0,0 +1,3 @@
+(ns shopping-list.core)
+
+(.log js/console "Hey sup dude?")
