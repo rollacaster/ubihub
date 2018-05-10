dfea5743d1a41af51ff4a74993f9cc1d30a210d1
Thomas Sojka <thomas.sojka@comsysto.com>
2018-05-10 18:18:44 +0200

    Use event sourcing in BE

--- src/shopping_list/core.clj
+++ src/shopping_list/core.clj
@@ -8,64 +8,66 @@
             [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.resource :refer [wrap-resource]]))
 
-(defonce app-db (atom {:shopping-list
-                       [["28dbf4c7-6d90-4ad3-90a5-8fad068d6464" {:quantity 1, :good "2dd89b67-3156-4c9e-8a44-7e4523e75199"}]
-                        ["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 8, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
-                        :goods
-                       {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}
-                        "2dd89b67-3156-4c9e-8a44-7e4523e75199" {:name "Milk"}
-                        "dbea6f63-1004-4c9c-8d28-b84c866df474" {:name "Cheese"}}}))
-(defn add-shopping-item
-  [uuid goodId app-db]
-  (update app-db
-         :shopping-list
-         #(cons (vector (str uuid) {:quantity 1 :good goodId}) %)))
+(defonce actions (atom [{:type :add-good :uuid "2dd89b67-3156-4c9e-8a44-7e4523e75199" :name "Milk"}
+                        {:type :add-good :uuid "dbea6f63-1004-4c9c-8d28-b84c866df474" :name "Cheese"}
+                        {:type :add-good :uuid "8280b3f8-e14a-45d9-be46-09ffb44b8db9":name "Banana"}]))
 
-(defn remove-shopping-item
-  [id app-db]
-  (update app-db :shopping-list (fn [x] (remove #(= (first %) id) x))))
+(defmulti reducer (fn [state action] (:type action)))
 
-(defn increase-quantity
-  [id app-db]
-  (update app-db :shopping-list
-          (fn [x] (map (fn [item]
-                         (if (= (first item) id)
-                           (vector id (update (second item) :quantity (comp #(min 9 %) inc)))
-                           item)) x))))
+(defmethod reducer :add-good [state action]
+  (let [{:keys [uuid name]} action]
+    (update state :goods #(assoc % uuid {:name name}))))
 
-(defn find-shopping-item
-  [id app-db]
-  (first (filter #(= (first %) id) (:shopping-list app-db))))
+(defmethod reducer :add-shopping-item [state action]
+  (let [{:keys [uuid good]} action]
+    (update state :shopping-list #(cons (vector (str uuid) {:quantity 1 :good good}) %))))
 
-(defn decrease-quantity
-  [id app-db]
-  (let [item (find-shopping-item id app-db)
+(defmethod reducer :remove-shopping-item [state action]
+  (let [{:keys [uuid]} action]
+    (update state :shopping-list (fn [x] (remove #(= (first %) uuid) x)))))
+
+(defmethod reducer :increase-quantity [state action]
+  (let [{:keys [uuid]} action]
+    (update state :shopping-list
+            (fn [x] (map (fn [item] (if (= (first item) uuid)
+                                      (vector uuid (update (second item)
+                                                           :quantity (comp #(min 9 %) inc)))
+                                      item)) x)))))
+
+(defmethod reducer :decrease-quantity [state action]
+  (let [{:keys [uuid]} action
+        item (first (filter #(= (first %) uuid) (:shopping-list state)))
         quantity (:quantity (second item))]
     (if (> quantity  1)
-      (update app-db :shopping-list
+      (update state :shopping-list
               (fn [x] (map (fn [item]
-                             (if (= (first item) id)
-                               (vector id (update (second item) :quantity (comp #(min 9 %) dec)))
+                             (if (= (first item) uuid)
+                               (vector uuid (update (second item) :quantity (comp #(min 9 %) dec)))
                                item)) x)))
-      (remove-shopping-item id app-db))))
+      (reducer state {:type :remove-shopping-item :uuid uuid}))))
+
+(defn compute-state
+  [actions]
+  (reduce reducer {:shopping-list [] :goods {}} actions))
 
 (defn send-edn [x]
-  (-> x
-      pr-str
-      response
-      (content-type "application/edn")))
+  (-> x pr-str response (content-type "application/edn")))
+
+(defn update-state
+  [action]
+  (-> (swap! actions #(conj % action)) compute-state send-edn))
 
 (defroutes app-routes
   (GET "/shopping-list" []
-       (-> @app-db send-edn))
+       (-> @actions compute-state send-edn))
   (POST "/shopping-list" [goodId]
-        (-> (swap! app-db #(add-shopping-item (java.util.UUID/randomUUID) goodId %)) send-edn))
+        (update-state {:type :add-shopping-item :uuid (java.util.UUID/randomUUID) :good goodId}))
   (POST "/shopping-list/:goodId/increase" [goodId]
-        (-> (swap! app-db #(increase-quantity goodId %)) send-edn))
+        (update-state {:type :increase-quantity :uuid goodId}))
   (POST "/shopping-list/:goodId/decrease" [goodId]
-        (->  (swap! app-db #(decrease-quantity goodId %)) send-edn))
+        (update-state {:type :decrease-quantity :uuid goodId}))
   (DELETE "/shopping-list/:goodId" [goodId]
-          (-> (swap! app-db #(remove-shopping-item goodId %)) send-edn))
+          (update-state {:type :remove-shopping-item :uuid goodId}))
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app
--- test/shopping_list/core_test.clj
+++ test/shopping_list/core_test.clj
@@ -2,58 +2,60 @@
   (:require [clojure.test :refer :all]
             [shopping-list.core :refer :all]))
 
-(deftest core
+(deftest shopping-items
   (testing "add-shopping-item"
     (is (=
-         (add-shopping-item "59b79451-6d22-44ec-93fc-8336bed7a46c"
-                            "8280b3f8-e14a-45d9-be46-09ffb44b8db9"
-                            {:shopping-list []
-                             :goods
-                             {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})
+         (reducer {:shopping-list []
+                   :goods
+                   {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}
+                  {:type :add-shopping-item
+                   :uuid "59b79451-6d22-44ec-93fc-8336bed7a46c"
+                   :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"})
          {:shopping-list
           [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
           :goods
           {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})))
   (testing "remove-shopping-item"
     (is (=
-         (remove-shopping-item "59b79451-6d22-44ec-93fc-8336bed7a46c"
-                               {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
-                                :goods
-                                {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})
+         (reducer {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
+                   :goods
+                   {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}
+                  {:type :remove-shopping-item :uuid "59b79451-6d22-44ec-93fc-8336bed7a46c"})
          {:shopping-list []
           :goods {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})))
   (testing "increase-quantity"
     (is (=
-         (increase-quantity "59b79451-6d22-44ec-93fc-8336bed7a46c"
-                            {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
-                             :goods
-                             {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})
+         (reducer {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
+                   :goods
+                   {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}
+                  {:type :increase-quantity :uuid "59b79451-6d22-44ec-93fc-8336bed7a46c"})
          {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 2, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
           :goods
           {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})))
   (testing "decrease-quantity"
     (testing "<=1 quantity"
       (is (=
-           (decrease-quantity "59b79451-6d22-44ec-93fc-8336bed7a46c"
-                              {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
-                               :goods
-                               {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})
+           (reducer {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
+                     :goods
+                     {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}
+                    {:type :decrease-quantity :uuid "59b79451-6d22-44ec-93fc-8336bed7a46c"})
            {:shopping-list []
             :goods
             {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}))))
   (testing ">1 quantity"
       (is (=
-           (decrease-quantity "59b79451-6d22-44ec-93fc-8336bed7a46c"
-                              {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 2, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
-                               :goods
-                               {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})
+           (reducer {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 2, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
+                     :goods
+                     {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}
+                    {:type :decrease-quantity :uuid "59b79451-6d22-44ec-93fc-8336bed7a46c"})
            {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 1, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
             :goods
-            {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})))
-  (testing "find-shopping-item"
+            {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}))))
+
+(deftest goods
+  (testing "add-goods"
     (is (=
-         (find-shopping-item "59b79451-6d22-44ec-93fc-8336bed7a46c"
-                            {:shopping-list [["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 2, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]]
-                             :goods
-                             {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}})
-         ["59b79451-6d22-44ec-93fc-8336bed7a46c" {:quantity 2, :good "8280b3f8-e14a-45d9-be46-09ffb44b8db9"}]))))
+           (reducer {:shopping-list [] :goods {}}
+                    {:type :add-good :uuid "8280b3f8-e14a-45d9-be46-09ffb44b8db9" :name "Banana"})
+           {:shopping-list []
+            :goods {"8280b3f8-e14a-45d9-be46-09ffb44b8db9" {:name "Banana"}}}))))
