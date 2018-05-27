7207d1e419e4aa03114f5219803c14f776e9abda
Thomas Sojka <thomas.sojka@comsysto.com>
2018-05-27 19:06:33 +0200

    Show categories in ui

--- src/shopping_list/core.cljs
+++ src/shopping_list/core.cljs
@@ -13,27 +13,6 @@
 (defonce add-goods-modal-shown? (atom false))
 
 ;; -------------------------
-;; Queries
-(defn find-shopping-item
-  [shopping-list id]
-  (second (first (filter #(= (first %) id) shopping-list))))
-
-(defn get-shopping-item
-  [id app-db]
-  (let [{:keys [shopping-list goods]} app-db
-        listItem (find-shopping-item shopping-list id)
-        good (get goods (:good listItem))]
-    (merge {:id id} (assoc listItem :good (:name good)))))
-
-(defn find-good
-  [goodId app-db]
-  (first (filter #(= (:good (second %)) goodId) (:shopping-list app-db))))
-
-(defn filter-goods
-  [app-db]
-  (update app-db :goods (fn [goods] (remove (fn [good] (find-good (first good) app-db)) goods))))
-
-;; -------------------------
 ;; Actions
 
 (defonce ws-chan (atom nil))
@@ -51,10 +30,6 @@
    (.send @ws-chan msg)
    (throw (js/Error. "Websocket is not available!"))))
 
-(defn add-shopping-item
-  [goodId]
-  (send! {:type :add-shopping-item :uuid (str (random-uuid)) :good goodId}))
-
 (defn remove-shopping-item
   [goodId]
   (send! {:type :remove-shopping-item :uuid goodId}))
@@ -69,11 +44,7 @@
 
 (defn toogle-goods-modal
   []
-  (let [body (.-body js/document)
-        overflow (.-overflow (.-style body))]
-    (if (= overflow "hidden")
-      (set! (.-overflow (.-style body)) "")
-      (set! (.-overflow (.-style body)) "hidden")))
+  (js/scrollTo 0 0)
   (swap! add-goods-modal-shown? not))
 
 ;; -------------------------
@@ -93,29 +64,32 @@
    (quantity-button "-" #(decrease-quantity id))])
 
 (defn shopping-item
-  [item]
-  [:li {:key (:id item) :class "flex items-center lh-copy pa3 bb b--black-10"}
-   [:input {:id (:id item)
+  [{:keys [uuid name quantity]}]
+  [:li {:key uuid :class "flex items-center lh-copy pa3 bb b--black-10"}
+   [:input {:id uuid
             :type "checkbox"
             :checked false
-            :on-change #(remove-shopping-item (:id item))}]
-   [:label {:for (:id item) :class "f3 db black-70 pl3 flex-auto"} (:good item)]
-   (quantity-counter (:id item) (:quantity item))])
+            :on-change #(remove-shopping-item uuid)}]
+   [:label {:for uuid :class "f3 db black-70 pl3 flex-auto"} name]
+   (quantity-counter uuid quantity)])
+
+(defn good [{:keys [uuid name]}]
+  [:li {:class "dib mr1 mb2" :key uuid}
+   [:button {:class "f5 b db pa2 dim dark-gray ba b--black-20 bg-white"
+             :on-click #(do (increase-quantity uuid) (toogle-goods-modal))} name]])
 
 (defn add-goods-modal
   [goods]
   [:div
    {:class (str "w-100 vh-100 bg-white absolute flex-column justify-between ")
     :style {:transform (str "translateX(" (if @add-goods-modal-shown? 0 375) "px)")
-            :transition "transform 0.5s"
-            :top (.-scrollY js/window)}}
+            :transition "transform 0.5s"}}
    [:div {:class "center pa3"}
-    [:ul {:class "list ph3 pv4"}
-     (map (fn [good] [:li {:class "dib mr1 mb2"
-                           :key (first good)}
-                      [:button {:class "f4 b db pa2 dim dark-gray ba b--black-20 bg-white"
-                                :on-click #(do (add-shopping-item (first good)) (toogle-goods-modal))} (-> good second :name)]])
-          goods)]]
+    (map (fn [{:keys [category goods]}]
+           [:div {:key category}
+            [:h2 {:class "f6"} category]
+            [:ul (map good goods)]])
+         goods)]
    [:div {:class "flex justify-center"}
     [:button {:class "f5 pa2 mb2 ba white b--black-20 bg-black"
               :on-click toogle-goods-modal} "back"]]])
@@ -130,12 +104,17 @@
 (defn main []
   (let [app-db @app-db
         add-goods-modal-shown? @add-goods-modal-shown?
-        goods (:goods (filter-goods app-db))]
-    [:div {:class (when add-goods-modal-shown? "m0 h-100 overflow-hidden")}
-     [:div {:class "relative sans-serif mw5 center pa3"}
+        {:keys [goods shopping-list]} app-db]
+    [:div
+     [:div {:class (str "relative sans-serif mw5 center pa3 overflow-x-hidden"
+                        (if add-goods-modal-shown? " dn" " "))}
       [:ul {:class "list pl0 mt0 measure center"}
-       (map (comp shopping-item #(get-shopping-item (first %) app-db)) (:shopping-list app-db))]]
-     (add-button goods)
+       (map (fn [{:keys [category shopping-items]}]
+              [:div {:key category}
+               [:h2 {:class "f6"} category]
+               (map shopping-item shopping-items)])
+            shopping-list)]]
+     (when (not add-goods-modal-shown?) (add-button goods))
      (add-goods-modal goods)]))
 
 
